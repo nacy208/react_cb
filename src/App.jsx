61bbdc987808208551fa4829a2d08d6ed546cf07
nacy208 <sohaha1304@gmail.com>
2025-09-30 22:36:21 +0700

    video28

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+//component = html + js+css
+const MyComponent = () => {
+  return (
+    <div>
+      olivia & react
+    </div>
+  );
+}
+
 const App = () => {
   const [count, setCount] = useState(0)
 
@@ -17,6 +27,7 @@ const App = () => {
         </a>
       </div>
       <h1>Hello world & olivia </h1>
+      <MyComponent />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
