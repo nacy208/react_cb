e88d7e5683e04c56e654697f08e705de99baae49
nacy208 <sohaha1304@gmail.com>
2025-10-01 10:04:39 +0700

    video33

---
+++ src/components/todo/TodoData.jsx
@@ -0,0 +1,10 @@
+const TodoData = () => {
+    return (
+        <div className="todo-data">
+            <div>Learn React</div>
+            <div>Learn Nodejs</div>
+        </div>
+    )
+}
+
+export default TodoData;
---
+++ src/components/todo/TodoNew.jsx
@@ -0,0 +1,8 @@
+const TodoNew = () => {
+    return (<div className="todo-new">
+        <input type="text" placeholder="nhap viec can lam" />
+        <button>add</button>
+    </div>)
+}
+
+export default TodoNew;
