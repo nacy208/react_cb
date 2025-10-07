31e01159a4c6fa41b0ff7a3709720752d2a5f796
nacy208 <sohaha1304@gmail.com>
2025-10-07 08:04:35 +0700

    video36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -3,10 +3,25 @@ const TodoNew = (props) => {
     console.log('props from TodoNew:', props);
     const { addNewTodo } = props;
     addNewTodo();
+
+    const handleClick = () => {
+        console.log('click add new todo');
+    }
+    const handleChange = (e) => {
+        console.log('input changed:', e.target.value);
+    }
+
+    /* cach 2
+    const handleChange = (name) => {
+        console.log('input changed:', name);
+    }
+    */
     return (<div className="todo-new">
-        <input type="text" placeholder="check list todo" />
-        <button>add</button>
+        <input type="text" placeholder="check list todo" onChange={handleChange} />
+        <button style={{ cursor: "pointer" }} onClick={handleClick} >add</button>
     </div>)
 }
 
 export default TodoNew;
+
+// cach 2: <input type="text" placeholder="check list todo" onChange={() => onChange(e.target.value)}/>
