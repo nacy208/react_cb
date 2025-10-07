ec965072429f7c8cc0049e25c5b04b62075d6d58
nacy208 <sohaha1304@gmail.com>
2025-10-07 22:47:59 +0700

    video 47

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import TodoData from './components/todo/TodoData';
 import TodoNew from './components/todo/TodoNew';
 import reactLogo from './assets/react.svg';
 import { useState } from 'react';
+import Header from './components/layout/header.jsx';
+import Footer from './components/layout/footer.jsx';
+
 
 
 const App = () => {
@@ -29,6 +32,7 @@ const App = () => {
   }
   return (
     <>
+      <Header />
       <div className="todo-container">
         <div className="todo-title">Todo List</div>
       </div>
@@ -60,7 +64,7 @@ const App = () => {
         <div className='todo-image'>
           <img src={reactLogo} className='logo' alt="todo" />
         </div>}
-
+      <Footer />
     </>
   )
 }
